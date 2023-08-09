2947e6828e805b877ed437eff10b0754dbea8166
Guru Mahan <iosdeve2022@gmail.com>
2023-08-09 12:29:12 +0530

    Initial Commit

---
+++ ModularActPOC/ModularActPOC.h
@@ -0,0 +1,18 @@
+//
+//  ModularActPOC.h
+//  ModularActPOC
+//
+//  Created by Guru Mahan on 09/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModularActPOC.
+FOUNDATION_EXPORT double ModularActPOCVersionNumber;
+
+//! Project version string for ModularActPOC.
+FOUNDATION_EXPORT const unsigned char ModularActPOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModularActPOC/PublicHeader.h>
+
+
---
+++ ModularActPOC/ModularClass.swift
@@ -0,0 +1,14 @@
+//
+//  ModularClass.swift
+//  ModularActPOC
+//
+//  Created by Guru Mahan on 09/08/23.
+//
+
+import Foundation
+
+public class ModularValidator {
+    public func emailValidator(email: String) -> Bool {
+        return true
+    }
+}
